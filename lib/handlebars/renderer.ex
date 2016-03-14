0d73cf98f7637a14a96975bf0b3d99ea08a2ffde
Alex Veenendaal <alex@opendoorgonorth.com>
2016-03-14 13:15:29 +0000

    simple boolean blocks

--- lib/handlebars/renderer.ex
+++ lib/handlebars/renderer.ex
@@ -7,6 +7,21 @@ defmodule Handlebars.Renderer do
         {:ok, eval(expr,context) }
     end
 
+    defp eval( [{:mustache_open, line, term}|t], context) do
+        # IO.puts "looking for closing '#{term}' in '#{inspect(t)}'"
+        # split the forthcoming tokens into those that are part of the block, and everything that comes after
+        case find_block_close(t, term, [], []) do
+            {:ok, tokens, rest} -> 
+                if eval_boolean(context, term) do
+                    # IO.puts("ok good #{inspect(tokens ++ rest)} - #{inspect(context)}")
+                    eval( tokens ++ rest, context )
+                else
+                   eval( rest, context ) 
+                end
+            _ -> {:error, "closing #{term} not found"}
+        end
+    end
+
     defp eval( [h|t], context ) do
         eval(h,context) ++ eval(t,context)
     end
@@ -19,9 +34,28 @@ defmodule Handlebars.Renderer do
         text
     end
 
-    defp eval( {:mustache, line, data}, context ) do
-        to_char_list( Map.get( context, to_string(data), '' ) )
+    defp eval( {:mustache, line, term}, context ) do
+        to_char_list( Map.get( context, to_string(term), '' ) )
     end
 
+
+
+    defp find_block_close([{:mustache_close, line, term}|t], search_term, accum, buffer) when term == search_term do
+      {:ok, accum, t}
+    end
+
+    defp find_block_close([h|t], search_term, accum, buffer) do
+        # IO.puts "find_block_close #{inspect(accum++[h])}"
+        find_block_close(t, search_term, accum ++ [h], buffer)
+    end
+
+    defp find_block_close([], search_term, accum, buffer) do
+        {:error, "#{search_term} not found"}
+    end
+
+
+    defp eval_boolean( context, term ) do
+        value = Map.get(context, to_string(term))
+    end
 
 end
--- test/handlebars/renderer_test.exs
+++ test/handlebars/renderer_test.exs
@@ -13,9 +13,31 @@ defmodule RendererTest do
   end
 
   test "text with mustache" do
-    assert R.execute( 
-      [{:text, 1, 'foo '}, {:mustache, 1, 'msg'}, {:text, 1, ' baz'}], %{"msg" => "bar"} 
-      ) == {:ok, 'foo bar baz'}
+    expr = [{:text, 1, 'Goodbye\n'}, {:mustache, 1, 'adj'}, {:text,1,'\n'}, {:mustache, 1, 'noun'},{:text,1,'!'}] 
+    
+    assert R.execute(expr, %{"adj" => "cruel", "noun" => "world"}) 
+      == {:ok, 'Goodbye\ncruel\nworld!'}
   end
 
+  test "booleans" do
+    expr = [ 
+      {:mustache_open, 1, 'goodbye'},
+      {:text, 1, 'GOODBYE '},
+      {:mustache_close, 1, 'goodbye'},
+      {:text, 1, 'cruel '},
+      {:mustache, 1, 'noun'},
+      {:text, 1, '!'},
+    ]
+
+    # booleans show the contents when true
+    assert R.execute( expr, %{ "goodbye" => true, "noun" => "world"}) == {:ok,'GOODBYE cruel world!'}
+
+    # booleans do not show the contents when false
+    assert R.execute( expr, %{ "goodbye" => false, "noun" => "world"}) == {:ok,'cruel world!'}
+  end
+
+
+  # test "eval boolean" do
+  #   assert R.eval_boolean( %{"value" => true}, "value") == true
+  # end
 end
